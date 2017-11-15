8b67b6bcbcdcbe09ad653f0b8d43154cc8122216
Niel de la Rouviere <nieldlr@gmail.com>
2017-11-15 15:05:51 +0200

    Add a few more token FAQs

--- src/FAQ.js
+++ src/FAQ.js
@@ -26,6 +26,16 @@ class FAQ extends Component {
             <h4>How much can the beneficiary withdraw?</h4>
             <p>The beneficiary can only withdraw 10% from their pool each week.</p>
 
+            <h4 id="tokenization">What happens when I add tokenization to my contract?</h4>
+            <p>After each withdrawal, a staketree contract keeps track of how much each funder has contributed to the beneficiary. 
+            When a staktree contract is tokenized, this contribution amount, which is already kept track of, can be claimed as tokens. 
+            This allows the funders & beneficiary to use those tokens in any creative way possible. Tokenizing a contract cannot be reversed.</p>
+            
+            <h4>What are the token details?</h4>
+            <p>When you add tokenization to your contract, you'll be asked to provide some details for your token. These can also not be changed once they're added.</p>
+            <p>The token name can be anything you prefer, eg StakeTree Tokens. The symbol (up to five letters) is a shorthand for your token. For example Ethereum's symbol is ETH,
+            and Bitcoin's symbol BTC. The token decimal amount is up to how many decimals you'd want for your tokens. 18 is the default. It's the same amount that ether uses as well.</p>
+            
             <h4>I really like this project and have some ideas, how do I share this?</h4>
             <p>You can contact the founder, Niel, directly: nieldlr@gmail.com.</p>
           </div>
