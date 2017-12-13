2a6c20a8ae5b2ed7e5f3c56357d5bf50a4a5f07f
Niel de la Rouviere <nieldlr@gmail.com>
2017-12-13 11:42:16 +0200

    Add a script that allows me to locally publish .json files to IPFS Infura. This would be automated in the future through the site

--- scripts/publishToIPFS.js
+++ scripts/publishToIPFS.js
@@ -3,8 +3,8 @@ const path = require('path');
 const ipfsAPI = require('ipfs-api');
 const ipfs = ipfsAPI({host: 'ipfs.infura.io', port: '5001', protocol: 'https'});
 
-fs.readFile(path.resolve('./src/creators.json'), (err, res)=>{
-	ipfs.files.add([{path: 'creators.json', content: res}], function (err, files) {
+fs.readFile(path.resolve('./data/rhyslindmark.json'), (err, res)=>{
+	ipfs.files.add([{path: 'rhyslindmark.json', content: res}], function (err, files) {
 	  console.log(err, files);
 	  // 'files' will be an array of objects containing paths and the multihashes of the files added
 	});
